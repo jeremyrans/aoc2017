d4e315bf87d5039d1031b8ee751ec496bee24fea
Jeremy Rans <jrans@vendasta.com>
2017-12-05 23:31:13 -0600

    day 6 solutions rank 906/758 I've had better days :/

---
+++ day6/part1.py
@@ -0,0 +1,19 @@
+lines = [l.split() for l in open('input.txt').readlines()][0]
+lines = [int(i) for i in lines]
+
+prev_configs = []
+
+count = 0
+while lines not in prev_configs:
+    prev_configs.append(lines[:])
+    maxV = max(lines)
+    maxI = lines.index(maxV)
+
+    lines[maxI] = 0
+    for x in xrange(1, maxV+1):
+        t = (maxI + x) % len(lines)
+        lines[t] += 1
+
+    count += 1
+
+print count
