75b4e5a8a4c58ceaa44a37198c45ffe250fdcdc0
Jeremy Rans <jrans@vendasta.com>
2017-12-07 23:40:32 -0600

    refactor day8 to use a exec more clearly

--- day8/part1.py
+++ day8/part1.py
@@ -5,12 +5,6 @@ lines = [l for l in open('input.txt').readlines()]
 registers = collections.defaultdict(int)
 
 for line in lines:
-    l = line.split()
-    l[0] = "registers['%s']" % l[0]
-    l[1] = l[1].replace("dec", "-=")
-    l[1] = l[1].replace("inc", "+=")
-    l[4] = "registers['%s']" % l[4]
-    l.append("else 0")
-    exec(" ".join(l))
+    exec("%s else 0" % line.replace("dec", "-=").replace("inc", "+=")[:-1], globals(), registers)
 
 print max(v for k, v in registers.iteritems())
--- day8/part2.py
+++ day8/part2.py
@@ -3,17 +3,10 @@ import collections
 lines = [l for l in open('input.txt').readlines()]
 
 registers = collections.defaultdict(int)
-values = []
+m = 0
 
 for line in lines:
-    l = line.split()
-    reg = l[0]
-    l[0] = "registers['%s']" % reg
-    l[1] = l[1].replace("dec", "-=")
-    l[1] = l[1].replace("inc", "+=")
-    l[4] = "registers['%s']" % l[4]
-    l.append("else 0")
-    exec(" ".join(l))
-    values.append(registers[reg])
+    exec("%s else 0" % line.replace("dec", "-=").replace("inc", "+=")[:-1], globals(), registers)
+    m = max([v for v in registers.itervalues()] + [m])
 
-print max(values)
+print m
