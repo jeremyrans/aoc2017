49af5882012d1403108091b697a195def1fa6a4b
Jeremy Rans <jrans@vendasta.com>
2017-12-07 23:16:12 -0600

    day 8 solutions rank 471/421

---
+++ day8/part2.py
@@ -0,0 +1,39 @@
+import collections
+
+import operator
+
+lines = [l for l in open('input.txt').readlines()]
+
+registers = {}
+instructions = []
+
+ops = {
+    '>': operator.gt,
+    '<': operator.lt,
+    '>=': operator.ge,
+    '<=': operator.le,
+    '==': operator.eq,
+    '!=': operator.ne
+}
+
+values = []
+
+for line in lines:
+    l = line.split()
+    reg = l[0]
+    op = ops[l[-2]]
+    cond_reg = l[-3]
+    delta = int(l[2]) if l[1] == "inc" else -int(l[2])
+
+    if reg not in registers:
+        registers[reg] = 0
+
+    if cond_reg not in registers:
+        registers[cond_reg] = 0
+
+    if op(registers[cond_reg], int(l[-1])):
+        registers[reg] += delta
+        values.append(registers[reg])
+
+
+print max(values)
