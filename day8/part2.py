e15f7710d7defdc2d7a39c8ffd80ec9a95b6dd06
Jeremy Rans <jrans@vendasta.com>
2017-12-07 23:31:12 -0600

    refactor day8 to use exec() which probably would have been a faster solution to come up with

--- day8/part2.py
+++ day8/part2.py
@@ -1,39 +1,19 @@
 import collections
 
-import operator
-
 lines = [l for l in open('input.txt').readlines()]
 
-registers = {}
-instructions = []
-
-ops = {
-    '>': operator.gt,
-    '<': operator.lt,
-    '>=': operator.ge,
-    '<=': operator.le,
-    '==': operator.eq,
-    '!=': operator.ne
-}
-
+registers = collections.defaultdict(int)
 values = []
 
 for line in lines:
     l = line.split()
     reg = l[0]
-    op = ops[l[-2]]
-    cond_reg = l[-3]
-    delta = int(l[2]) if l[1] == "inc" else -int(l[2])
-
-    if reg not in registers:
-        registers[reg] = 0
-
-    if cond_reg not in registers:
-        registers[cond_reg] = 0
-
-    if op(registers[cond_reg], int(l[-1])):
-        registers[reg] += delta
-        values.append(registers[reg])
-
+    l[0] = "registers['%s']" % reg
+    l[1] = l[1].replace("dec", "-=")
+    l[1] = l[1].replace("inc", "+=")
+    l[4] = "registers['%s']" % l[4]
+    l.append("else 0")
+    exec(" ".join(l))
+    values.append(registers[reg])
 
 print max(values)
